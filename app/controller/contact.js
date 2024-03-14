a16225100b02de88d3491bc297acfa0b74592b70
NellyDubois <nelly.dubois@oclock.school>
2024-03-14 10:56:09 +0100

    move nodemailer user to .env

--- app/controller/contact.js
+++ app/controller/contact.js
@@ -12,14 +12,14 @@ const contactController = {
     async sendEmail(req, res, next) {
 
         const transporter = nodemailer.createTransport({
-            service: `${process.env.NODEMAILER_SERVICE}`,
-            host: `${process.env.NODEMAILER_HOST}`,
-            port: `${process.env.NODEMAILER_PORT}`,
+            service: process.env.NODEMAILER_SERVICE,
+            host: process.env.NODEMAILER_HOST,
+            port: process.env.NODEMAILER_PORT,
             secure:true,    
 
             auth: {
-              user: `${process.env.NODEMAILER_USER}`,
-              pass: `${process.env.NODEMAILER_PASS}`
+              user: process.env.NODEMAILER_USER,
+              pass: process.env.NODEMAILER_PASS
             }
           });
 
